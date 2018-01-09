414f5c4a2d01a1d109dc8e3a46ab4220570886f9
Rahul Rapatwar <rahulrapatwar@yahoo.com>
2018-01-08 18:19:17 -0800

    created home page content and modified layout template

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Home page view
 @app.route('/')
 def index():
     return render_template('home.html')
+
+# About Us page
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
